8369a8d40e6c5a9b99274100125f34e8ddba3f7d
Antoine LIGEROT <antoine.ligerot@outlook.fr>
2023-02-08 16:58:48 +0100

    createcollegue2

--- src/app/shared/create-colleague-forms/create-colleague-forms.component.ts
+++ src/app/shared/create-colleague-forms/create-colleague-forms.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CollegueService} from "../../service/collegue.service";
 import {HttpClient} from "@angular/common/http";
 import {CollegueComponent} from "../components/collegue/collegue.component";
 import {ColleguePost} from "../models/colleguePost";
+import {LikeHate} from "../models/like-hate";
+
+
+let buttonCreateDisabled:boolean=false;
 @Component({
   selector: 'tc-create-colleague-forms',
   templateUrl: './create-colleague-forms.component.html',
   styleUrls: ['./create-colleague-forms.component.scss']
 })
+
 export class CreateColleagueFormsComponent {
    newCollegue:ColleguePost={pseudo:"gher", last:"reth", first:"reth", photo: "https://iili.io/HECdmoF.th.png"};
 
